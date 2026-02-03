746b397a0be7b40d0889b3985e3ee0d849e45291
Neto test <kitropllay@gmail.com>
2026-02-03 17:35:31 +0300

    Changes 2

---
+++ Lesson_2/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    double num1;
+    double num2;
+
+public:
+    bool set_num1(double n1) {
+        if (n1 != 0) {
+            num1 = n1;
+            return true;
+        }
+        return false;
+    }
+
+    bool set_num2(double n2) {
+        if (n2 != 0) {
+            num2 = n2;
+            return true;
+        }
+        return false;
+    }
+
+    double add() {
+        return num1 + num2;
+    }
+
+    double multiply() {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+
+    double divide_1_2() {
+        return num1 / num2;
+    }
+
+    double divide_2_1() {
+        return num2 / num1;
+    }
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Calculator calc;
+    double input;
+
+    while (true) {
+        cout << "Введите num1: ";
+        cin >> input;
+
+        if (!calc.set_num1(input)) {
+            cout << "Неверный ввод!" << endl;
+            continue;
+        }
+
+        cout << "Введите num2: ";
+        cin >> input;
+
+        if (!calc.set_num2(input)) {
+            cout << "Неверный ввод!" << endl;
+            continue;
+        }
+
+        cout << "num1 + num2 = " << calc.add() << endl;
+        cout << "num1 - num2 = " << calc.subtract_1_2() << endl;
+        cout << "num2 - num1 = " << calc.subtract_2_1() << endl;
+        cout << "num1 * num2 = " << calc.multiply() << endl;
+        cout << "num1 / num2 = " << calc.divide_1_2() << endl;
+        cout << "num2 / num1 = " << calc.divide_2_1() << endl;
+        cout << endl;
+    }
+
+}
---
+++ Lesson_2/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+class Counter {
+private:
+    int value;  
+
+public:
+    Counter() {
+        value = 1;
+    }
+
+    Counter(int startValue) {
+        value = startValue;
+    }
+
+    void increment() {
+        value++;
+    }
+
+    void decrement() {
+        value--;
+    }
+
+    int getValue() const {
+        return value;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string answer;
+    cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    cin >> answer;
+
+    Counter counter; 
+
+    if (answer == "да") {
+        int startValue;
+        cout << "Введите начальное значение счётчика: ";
+        cin >> startValue;
+        counter = Counter(startValue); 
+    }
+
+    char command;
+
+    do {
+        cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        cin >> command;
+
+        switch (command) {
+        case '+':
+            counter.increment();
+            break;
+        case '-':
+            counter.decrement();
+            break;
+        case '=':
+            cout << counter.getValue() << endl;
+            break;
+        case 'x':
+            cout << "До свидания!" << endl;
+            break;
+        default:
+            cout << "Неизвестная команда!" << endl;
+        }
+
+    } while (command != 'x');
+
+}
