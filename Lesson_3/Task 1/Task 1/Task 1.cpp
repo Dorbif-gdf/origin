6bcb1771b8cf32e7c7185a98b8ca6843a92656f3
Neto test <kitropllay@gmail.com>
2026-02-13 15:54:22 +0300

    Changes 3

---
+++ Lesson_3/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+class Address {
+private:
+    std::string city;
+    std::string street;
+    int house;
+    int apartment;
+
+public:
+    Address(std::string city, std::string street, int house, int apartment) {
+        this->city = city;
+        this->street = street;
+        this->house = house;
+        this->apartment = apartment;
+    }
+
+    std::string get_output_address() const {
+        return city + ", " + street + ", " +
+            std::to_string(house) + ", " +
+            std::to_string(apartment);
+    }
+};
+
+int main() {
+
+    std::ifstream in("in.txt");
+    std::ofstream out("out.txt");
+
+    if (!in.is_open() || !out.is_open()) {
+        std::cout << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
+    int N;
+    in >> N;
+    in.ignore(); 
+
+    Address** addresses = new Address * [N];
+
+    for (int i = 0; i < N; i++) {
+        std::string city;
+        std::string street;
+        int house;
+        int apartment;
+
+        std::getline(in, city);
+        std::getline(in, street);
+        in >> house;
+        in >> apartment;
+        in.ignore();
+
+        addresses[i] = new Address(city, street, house, apartment);
+    }
+
+    out << N << std::endl;
+
+    for (int i = N - 1; i >= 0; i--) {
+        out << addresses[i]->get_output_address() << std::endl;
+    }
+
+    in.close();
+    out.close();
+}
---
+++ Lesson_3/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Address {
+private:
+    string city;
+    string street;
+    int house;
+    int apartment;
+
+public:
+    Address(string city, string street, int house, int apartment) {
+        this->city = city;
+        this->street = street;
+        this->house = house;
+        this->apartment = apartment;
+    }
+
+    string get_output_address() const {
+        return city + ", " + street + ", " +
+            to_string(house) + ", " +
+            to_string(apartment);
+    }
+
+    string get_city() const {
+        return city;
+    }
+};
+
+void sort(Address** addresses, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (addresses[j]->get_city() > addresses[j + 1]->get_city()) {
+                Address* temp = addresses[j];
+                addresses[j] = addresses[j + 1];
+                addresses[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+
+    ifstream fin("in.txt");
+    ofstream fout("out.txt");
+
+    int N;
+    fin >> N;
+    fin.ignore();
+
+    // Массив указателей
+    Address** addresses = new Address * [N];
+
+    string city, street;
+    int house, apartment;
+
+    for (int i = 0; i < N; i++) {
+        getline(fin, city);
+        getline(fin, street);
+        fin >> house;
+        fin >> apartment;
+        fin.ignore();
+
+        addresses[i] = new Address(city, street, house, apartment);
+    }
+
+    sort(addresses, N);
+
+    fout << N << endl;
+    for (int i = 0; i < N; i++) {
+        fout << addresses[i]->get_output_address() << endl;
+    }
+
+    // Освобождение памяти
+    for (int i = 0; i < N; i++) {
+        delete addresses[i];
+    }
+    delete[] addresses;
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
