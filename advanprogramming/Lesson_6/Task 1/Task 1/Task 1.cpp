cc6ea02b97cc90d6dfc520a29b7403c49e0385bd
Neto test <kitropllay@gmail.com>
2026-05-05 18:04:25 +0300

    advp_lesson_6

---
+++ advanprogramming/Lesson_6/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n; 
+
+    std::vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> v[i];
+    }
+
+    std::sort(v.begin(), v.end());
+    auto it = std::unique(v.begin(), v.end());
+    v.erase(it, v.end());
+
+    for (int num : v) {
+        std::cout << num << " ";
+    }
+
+}
---
+++ advanprogramming/Lesson_6/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <string>
+
+template <typename Container>
+void print_container(const Container& c) {
+    for (auto it = c.begin(); it != c.end(); ++it) {
+        std::cout << *it;
+
+        auto next = it;
+        ++next;
+        if (next != c.end()) {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); 
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+}
