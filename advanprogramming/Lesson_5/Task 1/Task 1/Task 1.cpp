a0205e7e0527a581d059045b1722338c33eac125
Neto test <kitropllay@gmail.com>
2026-05-05 17:50:39 +0300

    advp_lesson_5

---
+++ advanprogramming/Lesson_5/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main() {
+    std::string text;
+    std::getline(std::cin, text);
+
+    std::map<char, int> freq;
+    for (char c : text) {
+        freq[c]++;
+    }
+
+    std::vector<std::pair<char, int>> vec(freq.begin(), freq.end());
+
+    std::sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
+        return a.second > b.second;
+        });
+
+    for (const auto& p : vec) {
+        std::cout << p.first << ": " << p.second << std::endl;
+    }
+
+}
---
+++ advanprogramming/Lesson_5/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::set<int, std::greater<int>> numbers;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        std::cin >> x;
+        numbers.insert(x);
+    }
+
+    for (int num : numbers) {
+        std::cout << num << std::endl;
+    }
+
+}
