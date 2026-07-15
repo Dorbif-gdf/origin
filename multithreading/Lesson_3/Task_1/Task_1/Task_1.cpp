4ab606217d60dcc7233986361729d1d69ec2b072
Neto test <kitropllay@gmail.com>
2026-07-15 15:25:10 +0300

    Lesson_3

---
+++ multithreading/Lesson_3/Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+void findMin(const std::vector<int>& arr,
+    int start,
+    std::promise<int> promise)
+{
+    int minIndex = start;
+
+    for (int i = start + 1; i < arr.size(); i++)
+    {
+        if (arr[i] < arr[minIndex])
+        {
+            minIndex = i;
+        }
+    }
+
+    promise.set_value(minIndex);
+}
+
+void selectionSort(std::vector<int>& arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        std::promise<int> promise;
+        std::future<int> future = promise.get_future();
+
+        auto task = std::async(
+            std::launch::async,
+            findMin,
+            std::cref(arr),
+            i,
+            std::move(promise)
+        );
+
+        int minIndex = future.get();
+
+        std::swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<int> arr = { 8, 5, 2, 9, 1, 7 };
+
+    std::cout << "Исходный массив:\n";
+
+    for (int x : arr)
+        std::cout << x << " ";
+
+    std::cout << "\n\n";
+
+    selectionSort(arr);
+
+    std::cout << "Отсортированный массив:\n";
+
+    for (int x : arr)
+        std::cout << x << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ multithreading/Lesson_3/Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+#include <algorithm>
+#include <iterator>
+
+template<typename Iterator, typename Function>
+void parallel_for_each(Iterator begin, Iterator end, Function func)
+{
+    auto length = std::distance(begin, end);
+
+    if (length <= 2)
+    {
+        for (; begin != end; ++begin)
+        {
+            func(*begin);
+        }
+        return;
+    }
+
+    Iterator middle = begin;
+    std::advance(middle, length / 2);
+
+    auto future = std::async(
+        std::launch::async,
+        parallel_for_each<Iterator, Function>,
+        begin,
+        middle,
+        func
+    );
+
+    parallel_for_each(middle, end, func);
+
+    future.get();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<int> numbers = { 1,2,3,4,5,6,7,8,9,10 };
+
+    parallel_for_each(
+        numbers.begin(),
+        numbers.end(),
+        [](int& value)
+        {
+            value *= 2;
+        });
+
+    std::cout << "Результат:\n";
+
+    for (int value : numbers)
+    {
+        std::cout << value << " ";
+    }
+
+    return 0;
+}
